00965c3c5de91d73f28811e1bd5fb4cfb5807ccf
favour <ojiakufavour@gmail.com>
2022-03-10 14:43:57 +0100

    tailwind css for a better ui

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ContactList } from './contactList';
 import { ContactForm } from './contactForm';
+import { Header } from './Header';
 
 
 // Contact: name, email, imageURL
 export const App = () => (
   <div>
-    <h1>Wallet using Meteor!</h1>
-    <ContactForm />
-    <ContactList />
+    <Header />
+    <div className="min-h-full">
+      <div className="max-w-4xl mx-auto p-2">
+        <ContactForm />
+        <ContactList />
+      </div>
+    </div>
+    
   </div>
 );
--- imports/ui/contactForm.jsx
+++ imports/ui/contactForm.jsx
@@ -14,27 +14,54 @@ export const ContactForm = () => {
         setImageUrl("");
     }
     return (
-        <form>
-            <div>
-                <label htmlFor='name'>
-                Name  
-                </label>
-                <input value={name} id='name' onChange={(e) => setName(e.target.value)} type="text" />
+        <form className="mt-6">
+            <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label htmlFor='name' className="block text-sm font-medium text-gray-700">
+                    Name  
+                    </label>
+                    <input 
+                        value={name} 
+                        id='name' 
+                        onChange={(e) => setName(e.target.value)} 
+                        type="text"
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus" />
+                </div>
+                
+                
             </div>
-            <div>
-                <label htmlFor='email'>
+            <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label htmlFor='email' className="block text-sm font-medium text-gray-700">
                     Email  
-                </label>
-                <input value={email} id='name' onChange={(e) => setEmail(e.target.value)} type="email" />
+                    </label>
+                    <input 
+                        value={email} 
+                        id='email' 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        type="email"
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus" />
+                </div>
+                
+                
+            </div>
+            <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label htmlFor='imageUrl' className="block text-sm font-medium text-gray-700">
+                    ImageUrl  
+                    </label>
+                    <input 
+                        value={imageUrl} 
+                        id='imageUrl' 
+                        onChange={(e) => setImageUrl(e.target.value)} 
+                        type="text"
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus" />
+                </div>
+                
+                
             </div>
             <div>
-                <label htmlFor='imageUrl'>
-                    Image URL  
-                </label>
-                <input value={imageUrl} id='imageUrl' onChange={(e) => setImageUrl(e.target.value)} type="text" />
-            </div>
-            <div>
-                <button type="button" onClick={saveContact}>Save Contact</button>
+                <button type="button" className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-content text-white" onClick={saveContact}>Save Contact</button>
             </div>
          </form>
     )
