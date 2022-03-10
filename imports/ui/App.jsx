741a961623195cbde1bce2ebed5960fc61ad52ce
favour <ojiakufavour@gmail.com>
2022-03-10 09:27:28 +0100

    update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,7 +6,7 @@ import { ContactForm } from './contactForm';
 // Contact: name, email, imageURL
 export const App = () => (
   <div>
-    <h1>Welcome Wallet Meteor!</h1>
+    <h1>Wallet using Meteor!</h1>
     <ContactForm />
     <ContactList />
   </div>
